c2346c22bd105d3a7704a55979499c19d6f08920
wesen <wesen1@users.noreply.github.com>
2021-07-02 15:08:52 +0200

    Add wavwriter

---
+++ source/src/wavwriter.h
@@ -0,0 +1,33 @@
+/**
+ * @author wesen
+ * @copyright 2021 wesen <wesen-ac@web.de>
+ */
+
+#ifndef WAV_WRITER
+#define WAV_WRITER
+
+class wavwriter
+{
+  private:
+    // Output file
+    char* outputFilePath;
+    std::ofstream *outputFile;
+    size_t dataChunkPos;
+
+    // WAV config
+    int numberOfSamplesPerSecond;
+    int numberOfChannels;
+    int numberOfBytesPerSample;
+    int sampleWordSize;
+
+    void initWavFile();
+    void finalizeWavFile();
+
+  public:
+    wavwriter(char*, int, int, int);
+    void init();
+    void writeSampleChannelData(int);
+    void finish();
+};
+
+#endif
